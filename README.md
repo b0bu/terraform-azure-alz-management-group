68e65c7316e7174b0b64fa62391d253737df487b
Jason Leitch <jasonleitch0@gmail.com>
2022-11-29 11:26:37 +0000

    chore: initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # terraform-azure-alz-management-group
 creates caf best practice management group hierarchy 
+
+#### example usage 
+https://github.com/b0bu/terraform-azure-alz-core-platform
---
+++ main.tf
@@ -0,0 +1,14 @@
+resource "azurerm_management_group" "mg" {
+  for_each = toset(var.management_groups)
+
+  display_name               = each.value
+  parent_management_group_id = var.parent_id
+}
+
+// map["management_group_name"] = management_group_id
+output "parent_id" {
+  value = {
+    for mg in toset(var.management_groups):
+    mg => azurerm_management_group.mg[mg].id
+  }
+}
