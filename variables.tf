035dbdd857282695d5074ae4c18773981dfcc9bd
Jason Leitch <jasonleitch0@gmail.com>
2022-12-01 14:27:43 +0000

    chore: add descr

--- variables.tf
+++ variables.tf
@@ -1,8 +1,10 @@
 variable "management_groups" {
     type = list
+    description = "a set of management group names to create"
 }
 
 variable "parent_id" {
     type = string
     default = null
+    description = "parent mg id defining the level at which the set of management_groups should be created"
 }
